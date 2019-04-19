04023cf40d9b6d4c396382b5bd607a9dbdeabc2e
fengf233 <38374173+fengf233@users.noreply.github.com>
2019-04-19 09:51:44 +0800

    add some

--- TR069-option.py
+++ TR069-option.py
@@ -17,28 +17,36 @@ def int2hex(i):
 def option_43(url,username,passwd):
     option43 = 'option vendor-encapsulated-options '+'01:'+int2hex(len(url))+str2hex(url)+':00:02:'+\
         int2hex(len(username)+len(passwd)+6)+":01:"+int2hex(len(username))+str2hex(username)+":00:02:"+\
-            int2hex(len(passwd))+str2hex(passwd)+":00"
+            int2hex(len(passwd))+str2hex(passwd)+":00;"
     return option43
 
 def option_125(url,username,passwd,idenfiy):
     option125 = 'option vendor-specific-information '+'00:00:0d:e9:'+int2hex(len(url)+len(username)+len(passwd)+len(idenfiy)+14)+ \
         ":0b:"+int2hex(len(url))+str2hex(url)+":0c:1a:01:"+int2hex(len(username))+str2hex(username)+":02:"+int2hex(len(passwd))+\
-            ":0D:01:36:0E:04:33:30:30:30:EA:"+int2hex(len(idenfiy))+str2hex(idenfiy)
+            ":0D:01:36:0E:04:33:30:30:30:EA:"+int2hex(len(idenfiy))+str2hex(idenfiy)+';'
     return option125
 
 def option_17(url,username,passwd,idenfiy):
     option17 = 'option dhcp6.vendor-opts '+'00:00:0D:E9:00:01:00:'+int2hex(len(url))+str2hex(url)+":00:02:00:"+\
-        int2hex(len(username)+len(passwd)+8)+":00:01:00:"+int2hex(len(username))+str2hex(username)+':00:02:00:'+\
-            str2hex(passwd)+":00:03:00:01:36:00:04:00:04:33:30:30:30:00:EA:00:"+int2hex(len(idenfiy))+str2hex(idenfiy)
+        int2hex(len(username)+len(passwd)+8)+":00:01:00:"+int2hex(len(username))+str2hex(username)+':00:02:00:'+int2hex(len(passwd))+\
+            str2hex(passwd)+":00:03:00:01:36:00:04:00:04:33:30:30:30:00:EA:00:"+int2hex(len(idenfiy))+str2hex(idenfiy)+';'
     return option17
 
+def w_file(option_list):
+    with open('option-list','w',encoding='utf-8') as f:
+        for option in option_list:
+            f.write('-----------'*4+'\n'*2)
+            f.write(option+'\n'*2)
 
 if __name__ == "__main__":
 
-    url ="http://www.baidu.com"
-    passwd = "dasfafsafasfaf"
-    user = "dadasffaf"
-    iden ="xxx"
+    url ="https://ACSServer"
+    passwd = "test"
+    user = "test"
+    iden ="ttt"
+    option_list = [option_43(url,user,passwd),option_125(url,user,passwd,iden),option_17(url,user,passwd,iden)]
+    w_file(option_list)
+    print(option_43(url,user,passwd))
     print(option_125(url,user,passwd,iden))
     print(option_17(url,user,passwd,iden))
-    print(option_43(url,user,passwd))
+
